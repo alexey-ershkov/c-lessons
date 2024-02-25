da1cc3211407d885503a9f577d49bb32549ee837
Alexey Erhskov <ershkov.alex2000@gmail.com>
2024-02-25 11:27:08 +0300

    update lesson 2

--- lesson_1/4_loops/main.c
+++ lesson_1/4_loops/main.c
@@ -1,30 +1,11 @@
 #include <stdio.h>
 
 int main() {
-    // for loop
-    printf("For loop\n");
-    for (int i = 0; i < 5; ++i) {
-        printf("%d ", i);
+
+
+    for (int i = 0, j = 1; 2; ++i, ++j) {
+        printf("some data");
     }
-
-    // while loop
-
-    printf("\nWhile loop\n");
-
-    int i = 0;
-    while (i < 5) {
-        printf("%d ", i);
-        ++i;
-    }
-
-    // do while loop
-
-    printf("\nDo while loop\n");
-    i = 6;
-
-    do {
-        printf("%d ", i);
-    } while (i < 5);
 
     return 0;
 }
--- lesson_2/04_dynamic_memory_allocation/main.c
+++ lesson_2/04_dynamic_memory_allocation/main.c
@@ -8,6 +8,7 @@ int main() {
     printf("Enter array length:\n");
     scanf("%d", &user_defined_length);
 
+    // calloc - то же самое, только с инициализацей нулями
     int* dynamic_int_array = malloc(user_defined_length * sizeof(int));
 
     for (int i = 0; i < user_defined_length; ++i) {
@@ -17,6 +18,8 @@ int main() {
     for (int i = 0; i < user_defined_length; ++i) {
         printf("Element %d of array is %d\n", i, dynamic_int_array[i]);
     }
+
+    free(dynamic_int_array);
 
     return 0;
 }
