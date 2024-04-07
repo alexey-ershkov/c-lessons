aff9ffff740fe7435ad74dff5a9c0a7df5053966
Alexey Erhskov <ershkov.alex2000@gmail.com>
2024-04-07 12:21:25 +0300

    add comments for vector

--- 3_c++_basic/4_strings_and_vectors/0_vectors.cpp
+++ 3_c++_basic/4_strings_and_vectors/0_vectors.cpp
@@ -30,19 +30,25 @@ int main() {
     printStringVector(v3);
 
     // props
+    // получить элемент массива
     std::cout << data[0] << " " << data.at(0) << std::endl;
+    // получить длину массива
     std::cout << "data size is " << data.size() << ", v3 size is " << v3.size() << std::endl;
 
+    // получить первый (front) и последний (back) элементы массива
     std::cout << "front is " << data.front() << ", back is " << data.back() << std::endl;
 
+    // получить информацию пустой ли массив
     std::cout << "data vector empty " << data.empty() << ", v2 vector empty is " << v2.empty() << std::endl;
 
     // add and delete
 
+    // Добавление в массив в конец
     data.push_back(7);
     printIntVector(data);
 
     while (data.size() > 2) {
+        // Удаление с конца массива
         data.pop_back();
     }
 
